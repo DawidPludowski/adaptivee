8c0860093ddf1a1b6b8a0a23773c17618e465e0e
DawidPludowski <dawid.pludowski@gmail.com>
2025-07-23 12:41:02 +0200

    add dryrun weights script

--- scripts/prepare_pretrianing_weights.sh
+++ scripts/prepare_pretrianing_weights.sh
@@ -4,39 +4,33 @@ export PYTHONPATH=`pwd`
 
 alphas=(
     1
-    # 0.1
-    # 10
+    0.1
+    10
+    inf
 )
 model_ids=("SIMPLE-1")
 
 input_paths=(
     "resources/liltab/split/encoder/test"
-    # "resources/liltab/split/encoder/train"
-    # "resources/liltab/split/inference/test"
-    # "resources/liltab/split/inference/train"
-    # "resources/Multitab/data-split/encoder/test"
-    # "resources/Multitab/data-split/encoder/train"
-    # "resources/Multitab/data-split/inference/test"
-    # "resources/Multitab/data-split/inference/train"
-    # "resources/tabzilla/datasets_split/encoder/test"
-    # "resources/tabzilla/datasets_split/encoder/train"
-    # "resources/tabzilla/datasets_split/inference/test"
-    # "resources/tabzilla/datasets_split/inference/train"
+    "resources/liltab/split/encoder/train"
+    "resources/liltab/split/inference/test"
+    "resources/liltab/split/inference/train"
+    "resources/Multitab/data-split/encoder/test"
+    "resources/Multitab/data-split/encoder/train"
+    "resources/Multitab/data-split/inference/test"
+    "resources/Multitab/data-split/inference/train"
+    "resources/tabzilla/datasets_split/encoder/test"
+    "resources/tabzilla/datasets_split/encoder/train"
+    "resources/tabzilla/datasets_split/inference/test"
+    "resources/tabzilla/datasets_split/inference/train"
 )
 
-for path in "${input_paths[@]}"; do
-    for id in "${model_ids[@]}"; do
-        for alpha in "${alphas[@]}"; do
-            echo "path=$path, id=$id, alpha=$alpha"
-            python bin/data/prepare_pretraining_weights.py \
-                --model-list-id $id \
-                --input-path $path \
-                --alpha $alpha
-        done
-        echo "path=$path, id=$id, onehot"
-        python bin/data/prepare_pretraining_weights.py \
-            --model-list-id $id \
-            --input-path $path \
-            --use-onehot
-    done
-done
+export ALPHAS="${alphas[*]}"
+export MODELS="${model_ids[*]}"
+export INPUTS="${input_paths[*]}"
+
+n_tasks=(( ${#alphas[@]} * ${#model_ids[@]} * ${#input_paths[@]} ))
+
+export ALPHAS MODELS INPUTS
+
+sbatch --export=ALL --array=0-$((n_tasks - 1)) bin/data/prepare_pretraining_weights.slurm
