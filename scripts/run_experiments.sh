4b6c25b34bba5ca8cbe5da20bf6551a3e86eb6d2
DawidPludowski <dawid.pludowski@gmail.com>
2025-07-31 18:59:42 +0200

    update exp config

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -2,7 +2,7 @@
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-17:13:53/model_checkpoints/model-epoch=9999-val_loss=-0.31.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/small
 export model_list_id="SIMPLE-1"
 export alpha="inf"
 export reweighter=simple
@@ -12,27 +12,7 @@ sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-17:13:53/model_checkpoints/model-epoch=9999-val_loss=-0.31.ckpt"
-export out_path=results-analysis/final
-export model_list_id="SIMPLE-1"
-export alpha="inf"
-export reweighter="direction-0.1"
-export n_iter=10
-
-sbatch --export=ALL ./scripts/run_experiments.slurm
-
-export data_dir=resources/liltab/split/inference
-export encoder_path="results/07-30-2025-17:13:53/model_checkpoints/model-epoch=9999-val_loss=-0.31.ckpt"
-export out_path=results-analysis/final
-export model_list_id="SIMPLE-1"
-export alpha="inf"
-export reweighter="direction-0.5"
-export n_iter=10
-
-sbatch --export=ALL ./scripts/run_experiments.slurm
-
-export data_dir=resources/liltab/split/inference
-export encoder_path="results/07-30-2025-17:13:53/model_checkpoints/model-epoch=9999-val_loss=-0.31.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/small
 export model_list_id="SIMPLE-1"
 export alpha="1.0"
 export reweighter=simple
@@ -41,31 +21,8 @@ export n_iter=10
 sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
-export encoder_path="results/07-30-2025-17:13:53/model_checkpoints/model-epoch=9999-val_loss=-0.31.ckpt"
-export out_path=results-analysis/final
-export model_list_id="SIMPLE-1"
-export alpha="1.0"
-export reweighter="direction-0.1"
-export n_iter=10
-
-sbatch --export=ALL ./scripts/run_experiments.slurm
-
-export data_dir=resources/liltab/split/inference
-export encoder_path="results/07-30-2025-17:13:53/model_checkpoints/model-epoch=9999-val_loss=-0.31.ckpt"
-export out_path=results-analysis/final
-export model_list_id="SIMPLE-1"
-export alpha="1.0"
-export reweighter="direction-0.5"
-export n_iter=10
-
-sbatch --export=ALL ./scripts/run_experiments.slurm
-
-
-
-
-export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/big
 export model_list_id="SIMPLE-1"
 export alpha="inf"
 export reweighter=simple
@@ -75,7 +32,17 @@ sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/big
+export model_list_id="SIMPLE-1"
+export alpha="inf"
+export reweighter=simple
+export n_iter=100
+
+sbatch --export=ALL ./scripts/run_experiments.slurm
+
+export data_dir=resources/liltab/split/inference
+export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
+export out_path=results-analysis/final/big
 export model_list_id="SIMPLE-1"
 export alpha="inf"
 export reweighter="direction-0.1"
@@ -85,7 +52,7 @@ sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/big
 export model_list_id="SIMPLE-1"
 export alpha="inf"
 export reweighter="direction-0.5"
@@ -95,7 +62,7 @@ sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/big
 export model_list_id="SIMPLE-1"
 export alpha="1.0"
 export reweighter=simple
@@ -105,7 +72,7 @@ sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/big
 export model_list_id="SIMPLE-1"
 export alpha="1.0"
 export reweighter="direction-0.1"
@@ -115,7 +82,7 @@ sbatch --export=ALL ./scripts/run_experiments.slurm
 
 export data_dir=resources/liltab/split/inference
 export encoder_path="results/07-30-2025-21:49:05/model_checkpoints/last.ckpt"
-export out_path=results-analysis/final
+export out_path=results-analysis/final/big
 export model_list_id="SIMPLE-1"
 export alpha="1.0"
 export reweighter="direction-0.5"
